7865fe77c84099c9b3d8e97218ceafaf9a45aafb
ThatGuyHyperz <69090660+Itz-Hyperz@users.noreply.github.com>
2021-08-02 20:02:36 -0700

    Add files via upload

---
+++ examplebot/commands/creator.js
@@ -0,0 +1,16 @@
+module.exports = {
+    name: 'creator',
+    description: 'A Command.',
+    aliases: ['credits', 'hyperz'],
+    async execute(client, message, args, Hyperz, config){
+        const pingEmbed = new Hyperz.MessageEmbed()
+        .setColor(config["main_config"].colorhex)
+        .setAuthor(`${message.author.tag}`, `${message.author.displayAvatarURL()}`, `${config["other_configuration"].serverinvite}`)
+        .setDescription(`**Created By:** [Hyperz#0001](https://hyperz.dev/)`)
+        .setTimestamp()
+        .setFooter(`${config["main_config"].copyright}`)
+        
+        message.channel.send(pingEmbed).then(msg => msg.delete({ timeout: 10000 })).catch(e => {if(config["main_config"].debugmode) return console.log(e);});
+        message.delete().catch(e => {if(config["main_config"].debugmode) return console.log(e);});
+    },
+}
---
+++ examplebot/commands/help.js
@@ -0,0 +1,56 @@
+const pagination = require('discord.js-pagination')
+const { setSizeDependencies } = require('mathjs')
+
+module.exports = {
+    name: 'help',
+    description: 'Shows all commands for the bot.',
+    aliases: ['helpmenu', 'helpme'],
+    async execute(client, message, args, Hyperz, config){
+
+        const page1 = new Hyperz.MessageEmbed()
+        .setTitle(`${client.user.username} Help Menu`)
+        .setColor(`${config["main_config"].colorhex}`)
+        .setAuthor(`${message.author.tag}`, `${message.author.displayAvatarURL()}`, `${config["other_configuration"].serverinvite}`)
+        .setThumbnail(`${client.user.displayAvatarURL()}`)
+        .setURL(`${config["other_configuration"].serverinvite}`)
+        .addFields(
+            {name: `Bot Name:`, value: `\`${client.user.username}\``, inline: true},
+            {name: `Bot Prefix:`, value: `\`${config["main_config"].prefix}\``, inline: true},
+            {name: `Server Owner:`, value: `${message.guild.owner.user.tag}`, inline: true},
+            {name: `Copyright:`, value: `${config["main_config"].copyright}\n \n`, inline: true},
+        )
+        .setTimestamp()
+        
+        const page2 = new Hyperz.MessageEmbed()
+        .setTitle(`User Commands`)
+        .setColor(`${config["main_config"].colorhex}`)
+        .setAuthor(`${message.author.tag}`, `${message.author.displayAvatarURL()}`, `${config["other_configuration"].serverinvite}`)
+        .setThumbnail(`${client.user.displayAvatarURL()}`)
+        .setDescription("`play` - Play a number from coomwaymp3.\n`creator` - Learn who made the bot.\n`help` - Get a list of everything you need to know.\n`ping` - Check to see if the bot is online.\n`invite` - Get an invite to the server.\n`version` - Learn what the current version of the bot is.")
+        .setTimestamp()
+
+        const page3 = new Hyperz.MessageEmbed()
+        .setTitle(`Bot Credits`)
+        .setColor(`${config["main_config"].colorhex}`)
+        .setAuthor(`${message.author.tag}`, `${message.author.displayAvatarURL()}`, 'https://hyperz.dev/')
+        .setThumbnail(`${client.user.displayAvatarURL()}`)
+        .addFields(
+            {name: `Programming:`, value: "`Hyperz#0001` - Head Developer of the HypeBot Project."},
+        )
+        .setTimestamp()
+
+        const pages = [
+            page1,
+            page2,
+            page3
+        ]
+
+        const emoji = ["⬅️", "➡️"]
+
+        const timeout = '100000'
+
+        pagination(message, pages, emoji, timeout)
+
+        message.delete().catch(e => {if(config["main_config"].debugmode) return console.log(e);});
+    }
+}
